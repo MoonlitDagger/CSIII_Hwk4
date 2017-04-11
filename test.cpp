5855c6cb2948da0ecd0d83d3012a6b617b86f90e
MoonlitDagger <anthange@gmail.com>
2017-04-11 17:17:24 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+//Anthony Hange, HWK_4
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+	string s1, s2;	//The two test words.
+
+	
+
+
+	if (s1.length() == s2.length())
+		;
+	else 
+		cout << "No word ladder exists for this pair of words.";
+
+	return EXIT_SUCCESS;
+}
